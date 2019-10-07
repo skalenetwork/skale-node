87f7e341ed297fd3c3ea8f9d172f1f6616605f7d
Oleh Nikolaiev <oleg@skalelabs.com>
2019-10-07 14:57:48 +0300

    SKALE-1644-fix-build-boost

--- scripts/build_dkgpython.sh
+++ scripts/build_dkgpython.sh
@@ -7,15 +7,32 @@ sudo apt-get install -y automake libboost-all-dev
 
 cd /tmp
 git clone https://github.com/skalenetwork/libBLS.git --recursive
-cd libBLS
+
+git clone git@github.com:boostorg/boost.git --recursive
+cd boost
+./bootstrap.sh
+./b2 link=static threading=multi runtime-link=static stage
+./b2 link=static threading=multi runtime-link=static install
+
+cd /tmp/libBLS
 
 mkdir -p build
 cd build
 cmake .. -DBUILD_WITH_FPIC=ON
+if [[ $? -ne 0 ]] ; then
+  exit 1
+fi
+
 make -j$(nproc) bls
+if [[ $? -ne 0 ]] ; then
+  exit 1
+fi
 
 cd ../python/
 bash setup.sh
+if [[ $? -ne 0 ]] ; then
+  exit 1
+fi
 
 mv build/lib.linux-x86_64-3.6/dkgpython.cpython-36m-x86_64-linux-gnu.so $NODE_DATA_DIR/dkgpython.so
 
